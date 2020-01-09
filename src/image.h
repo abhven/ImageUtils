ae1ef48e5c3b21679a4b0eb7f18a84ea41dbb720
Abhishek Venkataraman <abhven@umich.edu>
2020-01-09 00:50:40 -0500

    pipeline for reading jpeg data

---
+++ src/image.h
@@ -0,0 +1,76 @@
+#ifndef IMAGE_H
+#define IMAGE_H
+
+#include<vector>
+#include<string>
+#include<jpeglib.h>
+#include<string>
+
+class Image{
+    private:
+	std::vector< std::vector <uint8_t>> bitmap;
+	size_t     width;  /* Width of the image  */
+	size_t     height; /* Height of the image */
+	size_t     stride; /* physical row width in output buffer. Typically for RGB image stride is 3*width */
+	std::string 	   format; /* format of the bitmap */
+	int        state;
+    public:
+	Image (char * filename);
+	void print_info();
+	int blur_image(std::vector<std::vector<float>> kernel);
+
+};
+
+void Image::print_info(){
+	std::cout<<"\n---- IMAGE INFO ----\n";
+	std::cout<<"Height: "<<this->height<<"\nWidth: "<<this->width<<"\nFormat: "<<this->format;
+	std::cout<<"\n--------------------\n";
+}
+
+Image::Image (char * filename)
+{
+  struct jpeg_decompress_struct cinfo;
+  struct jpeg_error_mgr jerr;
+  /* More stuff */
+  FILE * infile;                /* source file */
+  JSAMPARRAY buffer;            /* Output row buffer */
+  int row_stride;               /* physical row widthdd in output buffer */
+
+  if ((infile = fopen(filename, "rb")) == NULL) {
+    fprintf(stderr, "can't open %s\n", filename);
+    this->state = -1; 
+    return;
+  }
+
+
+  cinfo.err = jpeg_std_error(&jerr);
+  jpeg_create_decompress(&cinfo);
+  jpeg_stdio_src(&cinfo, infile);
+
+
+  (void) jpeg_read_header(&cinfo, TRUE);
+
+  (void) jpeg_start_decompress(&cinfo);
+  this->width  = cinfo.output_width;
+  this->height = cinfo.output_height; 
+  this->stride = this->width * cinfo.output_components;
+  this->bitmap.reserve(this->height);
+
+  while (cinfo.output_scanline < this->height) {
+	  std::vector<uint8_t> line(this->stride);
+	 uint8_t *buffer = line.data();
+    	 jpeg_read_scanlines(&cinfo, &buffer, 1);
+	 this->bitmap.push_back(line);
+  }
+
+
+  (void) jpeg_finish_decompress(&cinfo);
+  jpeg_destroy_decompress(&cinfo);
+
+  fclose(infile);
+
+  this->state = 1;
+  this->format = "RGB";
+}
+
+#endif  /* IMAGE_H */
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,61 @@
 #include<iostream>
+#include<jpeglib.h>
+#include<string>
+#include<stdio.h>
+#include"image.h"
 
 using namespace std;
 
-int main()
+// Forward declaration
+int read_JPEG_file (char * filename);
+
+int main(int argc, char *argv[])
 {
-	cout<<"Hello World!\n";
+	if(argc > 1){
+		Image im(argv[1]);
+		im.print_info();
+	}
 	return 0;
 }
+
+
+int read_JPEG_file (char * filename)
+{
+  struct jpeg_decompress_struct cinfo;
+  struct jpeg_error_mgr jerr;
+  /* More stuff */
+  FILE * infile;		/* source file */
+  JSAMPARRAY buffer;		/* Output row buffer */
+  int row_stride;		/* physical row width in output buffer */
+
+  if ((infile = fopen(filename, "rb")) == NULL) {
+    fprintf(stderr, "can't open %s\n", filename);
+    return 0;
+  }
+
+
+  cinfo.err = jpeg_std_error(&jerr);
+  jpeg_create_decompress(&cinfo);
+  jpeg_stdio_src(&cinfo, infile);
+
+
+  (void) jpeg_read_header(&cinfo, TRUE);
+
+  (void) jpeg_start_decompress(&cinfo);
+  row_stride = cinfo.output_width * cinfo.output_components;
+  buffer = (*cinfo.mem->alloc_sarray)
+		((j_common_ptr) &cinfo, JPOOL_IMAGE, row_stride, 1);
+
+  while (cinfo.output_scanline < cinfo.output_height) {
+    (void) jpeg_read_scanlines(&cinfo, buffer, 1);
+  }
+
+
+  (void) jpeg_finish_decompress(&cinfo);
+  jpeg_destroy_decompress(&cinfo);
+
+  fclose(infile);
+
+  return 1;
+}
+
